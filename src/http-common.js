168b07ade418c0c2e988e1cb43ab01f2ac7c6710
ducquang2 <quangbilun@gmail.com>
2021-01-23 17:35:11 +0700

    update port and baseurl

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080/api/",
+  baseURL: "http://192.168.1.204:8080/api/",
   headers: {
     "Content-type": "application/json"
   }
